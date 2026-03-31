c9227fa3d1a64b49700b817e686a1dd67a6bb2e8
Longgnguyen1 <nguyenvulongabc11@1gmail.1gmailcom>
2026-03-31 17:53:05 +0700

    choke:update migration

---
+++ src/main/resources/changelog/migrations/2024-06-01-create-users-table.sql
@@ -0,0 +1,13 @@
+-- liquibase formatted sql
+
+-- changeset ltc:2026031301
+-- comment: Create users table with Role as Enum (No Foreign Key)
+CREATE TABLE IF NOT EXISTS users (
+    -- Đổi BIGINT thành VARCHAR(36) để chứa chuỗi UUID
+    id VARCHAR(36) PRIMARY KEY, 
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+-- rollback DROP TABLE users;
